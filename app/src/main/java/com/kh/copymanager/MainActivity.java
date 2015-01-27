ef12102b0e94d8278c6f4c8fac550e9a2aaf3df0
Hyun Kim <cyber7eyes@gmail.com>
2015-01-28 00:36:33 +0900

    setting

--- app/src/main/java/com/kh/copymanager/MainActivity.java
+++ app/src/main/java/com/kh/copymanager/MainActivity.java
@@ -2,16 +2,21 @@ package com.kh.copymanager;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.InjectView;
+
 
 public class MainActivity extends ActionBarActivity {
-
+    @InjectView(R.id.main)
+    RecyclerView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
